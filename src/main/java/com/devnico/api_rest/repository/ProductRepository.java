b382ac3812a398a20d58b6eafb0bce4176b4ecb5
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-08 21:11:54 -0500

    Product Repository

---
+++ src/main/java/com/devnico/api_rest/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.devnico.api_rest.repository;
+import com.devnico.api_rest.entity.Product;
+import com.devnico.api_rest.entity.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByProductName(String productName);
+
+    List<Product> findByStatus(Status status);
+
+}
