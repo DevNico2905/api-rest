9cdd9ca10fa56372df6288cd686e11c7b9e8c9da
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-12 10:55:53 -0500

    Constrains added to Product Entity.

--- src/main/java/com/devnico/api_rest/entity/Product.java
+++ src/main/java/com/devnico/api_rest/entity/Product.java
@@ -1,9 +1,15 @@
 package com.devnico.api_rest.entity;
 import com.devnico.api_rest.entity.enums.Status;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -11,26 +17,43 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
 
+    // Importante. Hay validaciones DDL. Y otras como los constrains.
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 100)
+    @NotBlank // <-- Para Strings
+    @Size(max = 100)
+    @Column(name = "name", nullable = false, length = 100, unique = true)
     private String productName;
 
+    @Size(max = 500)
     @Column( length = 500)
     private String description;
 
+    //Se considera que para precio es sumamente importante el BigDecimal
+    @NotNull // <-- Para numbers
     @Column(nullable = false)
-    private Double price;
+    // private Double price; <-- Not!!
+    private BigDecimal price;
 
+    @NotNull
+    @PositiveOrZero
     @Column(nullable = false)
-    private int amount;
+    // private int amount; <-- Not!
+    private Integer amount;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
 
     // Algo muy importante es marca los nullable, length y pilas con los Enums
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_category", referencedColumnName = "id_category")
+    private Category category;
 
 }
