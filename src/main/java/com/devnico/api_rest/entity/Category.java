066a51916e23531ea24a50c44b97a3b315bab5df
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-12 10:55:39 -0500

    Constrains added to Category Entity.

---
+++ src/main/java/com/devnico/api_rest/entity/Category.java
@@ -0,0 +1,28 @@
+package com.devnico.api_rest.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id_category")
+   private Long idCategory;
+
+   @NotBlank
+   @Size(max = 100)
+   @Column(nullable = false, length = 100, unique = true)
+    private String categoryName;
+
+
+}
