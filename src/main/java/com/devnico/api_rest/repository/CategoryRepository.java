411eb5f499822d69bcd3724b7d22e6208a0d6792
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-17 09:18:48 -0500

    New repo queries.

--- src/main/java/com/devnico/api_rest/repository/CategoryRepository.java
+++ src/main/java/com/devnico/api_rest/repository/CategoryRepository.java
@@ -1,7 +1,15 @@
 package com.devnico.api_rest.repository;
-
 import com.devnico.api_rest.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByCategoryName(String categoryName);
+
+    boolean existsByCategoryName(String categoryName);
+
 }
